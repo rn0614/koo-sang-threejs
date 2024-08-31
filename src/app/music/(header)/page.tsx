1ce9f1d32c1f1efb4b784dc37ff6675ee28e3632
rn0614 <rn0614@naver.com>
2024-08-31 20:45:01 +0900

    fix breaking-point scss 공통변수로 수정

--- src/app/music/(header)/page.tsx
+++ src/app/music/(header)/page.tsx
@@ -1,6 +1,7 @@
 import getSongs from "@/actions/getSongs";
 import { AddFloatButton } from "@/components/AddFloatButton/AddFloatButton";
 import PageContent from "@/components/PageContent/PageContent";
+//import {faker} from '@faker-js/faker';
 
 export default async function Home() {
   const songs = await getSongs();
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,14 @@
-import styles from "./page.module.scss";
-import getSongs from "@/actions/getSongs";
-import Head from "next/head";
+import styles from "./styles.module.scss";
 import HomeContent from "@/components/HomeContent/page";
 
+export const metadata = {
+  title: "Koosang Project",
+  description:"koo Sangmo의 놀이터입니다."
+}
+
 export default async function Home() {
-  const songs = await getSongs();
   return (
     <main className={styles.flexColumn}>
-      <Head>
-        <title>KooSang Project</title>
-        <meta name="home page for koosang-project v2" content="koosang-project의 3번째"></meta>
-      </Head>
       <HomeContent/>
     </main>
   );
